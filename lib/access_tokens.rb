9d3816ee9ef1ece4a60af5494c6de6102a5d6875
David Blinn <dblinn@cyrusinnovation.com>
2015-01-19 12:03:47 -0500

    Access token generation.

---
+++ lib/access_tokens.rb
@@ -0,0 +1,15 @@
+module Shortly
+  class AccessTokens
+    ACCESS_TOKEN_LENGTH = 8
+    ACCESS_TOKEN_REGEX = /^[a-z0-9]{8}$/
+
+    def self.generate
+      # Adapted from http://zh.soup.io/post/36288765/How-to-create-small-unique-tokens-in
+      rand(36**ACCESS_TOKEN_LENGTH).to_s(36).center(8, '0')
+    end
+
+    def self.valid?(access_token)
+      (access_token =~ ACCESS_TOKEN_REGEX) != nil
+    end
+  end
+end
---
+++ spec/access_tokens_spec.rb
@@ -0,0 +1,33 @@
+require './spec/spec_helper'
+require './lib/access_tokens'
+
+describe 'Shortly::AccessTokens' do
+  describe '#generate' do
+    it 'should generate strings' do
+      expect(Shortly::AccessTokens.generate).to be_kind_of(String)
+    end
+  end
+
+  describe '#valid' do
+    it 'should validate lower-case alphanumeric strings of the correct length' do
+      expect(Shortly::AccessTokens.valid?('12345678')).to be_truthy
+    end
+
+    it 'should invalidate strings of the incorrect length' do
+      expect(Shortly::AccessTokens.valid?('123456789')).to be_falsey
+    end
+
+    it 'should invalidate strings with incorrect characters' do
+      expect(Shortly::AccessTokens.valid?('1234567*')).to be_falsey
+    end
+  end
+
+  context '#generate and #valid integration' do
+    it 'should not generate invalid tokens' do
+      # 1000000.times do
+      100000.times do |i|
+        expect(Shortly::AccessTokens.valid?(Shortly::AccessTokens.generate)).to be_truthy
+      end
+    end
+  end
+end
