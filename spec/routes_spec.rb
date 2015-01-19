78f9f5db85b5daf59dae127ad85348fd97a7b6e7
David Blinn <dblinn@cyrusinnovation.com>
2015-01-19 14:42:52 -0500

    Put routes test in Shortly module

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -1,31 +1,33 @@
 require './spec/spec_helper'
 require './application'
 
-describe 'Shortly::Application' do
-  def app
-    Shortly::Application
-  end
-
-  describe 'GET "/"' do
-    it 'serves the root url' do
-      get '/'
-      expect(last_response).to be_ok
-      expect(last_response.body).to match('shorts-logo')
-      expect(last_response.body).to match('/top_hundred')
+module Shortly
+  describe 'Application' do
+    def app
+      Application
     end
-  end
 
-  describe 'GET "/top_hundred"' do
-    it 'serves the route for the top hundred' do
-      get '/top_hundred'
-      expect(last_response).to be_ok
+    describe 'GET "/"' do
+      it 'serves the root url' do
+        get '/'
+        expect(last_response).to be_ok
+        expect(last_response.body).to match('shorts-logo')
+        expect(last_response.body).to match('/top_hundred')
+      end
     end
-  end
 
-  describe 'POST "/shorten"' do
-    it 'serves the post route for url shortening' do
-      post '/shorten', { url: 'www.nytimes.com' }
-      expect(last_response).to be_ok
+    describe 'GET "/top_hundred"' do
+      it 'serves the route for the top hundred' do
+        get '/top_hundred'
+        expect(last_response).to be_ok
+      end
+    end
+
+    describe 'POST "/shorten"' do
+      it 'serves the post route for url shortening' do
+        post '/shorten', { url: 'www.nytimes.com' }
+        expect(last_response).to be_ok
+      end
     end
   end
 end
